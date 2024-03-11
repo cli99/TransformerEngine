fixed incorrect # of gradients
Signed-off-by: Alp Dener <adener@nvidia.com>

transformer_engine/pytorch/module/layernorm_linear.py
@@ -368,7 +368,6 @@ class _LayerNormLinear(torch.autograd.Function):
                 ctx.fsdp_shapes,
                 mu,
                 rsigma,
-                main_grad,
                 weight_t_fp8,
                 ln_out
             )
@@ -640,6 +639,7 @@ class _LayerNormLinear(torch.autograd.Function):
             None,
             None,
             grad_bias,
+            None,
             None,
             None,
             None,